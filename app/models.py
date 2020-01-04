ec106f44aede3b2530044963efa39319afd4e1d3
bhuwan neupane <bdr.bhuwan@gmail.com>
2020-01-04 11:54:48 +0545

    first commit

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+	name = models.CharField(max_length=40)
+	phone = models.IntegerField()
+	info = models.TextField(max_length = 200, blank=True)
+	email = models.EmailField(blank=True)
+	date = models.DateTimeField()
+	image = models.ImageField(upload_to="images/",blank=True)
+	gender = models.TextField(max_length = 40,choices=(('male','Male'),('female','Female')))
+
+	def __str__(self):
+		return self.name
+
+
