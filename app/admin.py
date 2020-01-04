bb5543eca5cd3df1f10bc462c8b92a144993c9f0
bhuwan neupane <bdr.bhuwan@gmail.com>
2020-01-04 13:14:26 +0545

    adding detail view functionality

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Contact
 
 # Register your models here.
-admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','name','gender','email','phone')
+    list_editable = ('email','phone')
+    list_per_page = 8
+    search_fields = ('name','gender','email','phone','info') 
+    
+admin.site.register(Contact,ContactAdmin)
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Contact
 
 # Create your views here.
 def index(request):
 	contacts = Contact.objects.all
 	return render(request,'index.html',{'contacts':contacts})
+
+def detail(request, id):
+	contacts = get_object_or_404(Contact, pk=id)
+	return render(request, 'detail.html',{'contacts':contacts})
