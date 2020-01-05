e33287206d72f2b3b4a56c7c84a15163d8219064
bhuwan neupane <bdr.bhuwan@gmail.com>
2020-01-05 16:38:50 +0545

    search functionality is added

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
 path('',views.HomeListView.as_view(),name='index'),
 path('detail/<int:pk>/',views.DetailDetailView.as_view(),name='detail'),
+path('search/',views.search,name='search'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Contact
 from django.views.generic import ListView,DetailView
+from django.db.models import Q
 
 # Create your views here.
 # def index(request):
@@ -18,3 +19,19 @@ class DetailDetailView(DetailView):
 # def detail(request, id):
 # 	contacts = get_object_or_404(Contact, pk=id)
 # 	return render(request, 'detail.html',{'contacts':contacts})
+
+def search(request):
+	if request.GET:
+		
+		search_term = request.GET['search_text']
+		search_result = Contact.objects.filter( Q(name__icontains=search_term)|
+		Q(email__icontains=search_term)|
+		Q(phone__iexact=search_term)|
+		Q(info__icontains=search_term)
+		)
+		context = {'search_term':search_term,
+		'contacts':search_result
+		}
+		return render(request,'search.html',context)
+	else:
+		return redirect('index')
